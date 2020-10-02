c56198b311acc821f6f513c7e90a44e8e6e68089
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-02 18:14:09 -0300

    style: change map

--- src/Components/Mapa/index.jsx
+++ src/Components/Mapa/index.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { Container } from "./styles";
 import Map from "pigeon-maps";
 import Marker from 'pigeon-marker'
+import MapaImage from '../../assets/MAPA/MAPA.png'
 
 export default function Mapa() {
   return (
     <Container>
       <h1>Mapa de Operações</h1>
       <h1 className="subtitle">Veja operações próximas a sua localização.</h1>
-      <Map twoFingerDrag={true} defaultZoom={12} center={[-23.5489,  -46.6388]} zoom={12}  height={400}>
+      <img src={MapaImage} alt="Mapa"/>
+      {/*
+<Map twoFingerDrag={true} defaultZoom={12} center={[-23.5489,  -46.6388]} zoom={12}  height={400}>
           <Marker anchor={[-23.5489,  -46.6388]} payload={1} onClick={({ event, anchor, payload }) => {}} />
       </Map>
+
+*/}   
     </Container>
   );
 }
--- src/Components/Mapa/styles.jsx
+++ src/Components/Mapa/styles.jsx
@@ -2,20 +2,35 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   text-align: center;
-  width: 100vw;
   font-weight: bold;
   margin: auto;
   margin-bottom: 64px;
   color: #fff;
+  h1{
+    margin-bottom: 32px;
+  }
+  img{
+    object-fit:cover;
+    object-position: center center;
+    width: 100%;
+    height: 450px;
+  }
 `;
 
 export const LinkWrapper = styled.div`
   background-color: #07d697;
+  object-fit: cover;
   color: black;
   padding: 16px 0px;
   border-radius: 10px;
-  width: 300px;
   margin: auto;
+  h1{
+    margin-bottom: 32px;
+  }
+  img{
+    object-fit: contain;
+    width: 100%;
+  }
   a{
     color: black;
     text-decoration: none;
