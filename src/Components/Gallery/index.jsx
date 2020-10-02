e6709bb81cc23c32ef1414f8e56091155a74fee4
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-02 11:11:15 -0300

    style: change pictures

--- src/Components/Gallery/index.jsx
+++ src/Components/Gallery/index.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Container, RowImage, Item, FullImage } from "./styles";
 import GenericsImage from "../../assets/generics.png";
+import DesafieSe from "../../assets/desafie-se.jpg"
+import ConvoqueSeusAmigos from "../../assets/convoqueSeusAmigosGallery.jpg"
+import Desempenho from "../../assets/seuDesempenhoGallery.jpg"
 
 const Gallery = () => {
   return (
     <Container>
       <RowImage>
         <Item>
-          <img src={GenericsImage} alt="Imagem Genérica" />
+          <img src={DesafieSe} alt="Desafie-se" />
           <p>Desafie-se</p>
         </Item>
         <Item>
-          <img src={GenericsImage} alt="Imagem Genérica" />
+          <img src={Desempenho} alt="Desempenho" />
           <p>Acompanhe seu desempenho</p>
         </Item>
         <Item>
-          <img src={GenericsImage} alt="Imagem Genérica" />
+          <img src={GenericsImage} alt="Operacoes" />
           <p>Fique por dentro das operações</p>
         </Item>
         <Item>
-          <img src={GenericsImage} alt="Imagem Genérica" />
+          <img src={ConvoqueSeusAmigos} alt="Convque Amigos" />
           <p>Convoque seus amigos</p>
         </Item>
       </RowImage>
--- src/Components/Gallery/styles.jsx
+++ src/Components/Gallery/styles.jsx
@@ -26,6 +26,7 @@ export const Item = styled.div`
   text-align: center;
   img {
     width: 100%;
+    height: 300px;
   }
 `;
 
