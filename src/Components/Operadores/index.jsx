636c42a3c5c0b6baa32b33cd28a9ae0f2b8aa4a5
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-07 19:51:57 -0300

    Change image organizador

--- src/Components/Operadores/index.jsx
+++ src/Components/Operadores/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Item, Content } from "./styles";
 import Operador from "../../assets/operador.png";
 import Capitao from "../../assets/capitao.png";
-import Generic from "../../assets/operacoes.png";
+import  organizador from "../../assets/organizador.png";
 
 const Operadores = () => {
   return (
@@ -21,7 +21,7 @@ const Operadores = () => {
           <p>Conquiste novos níveis evolua em combate.</p>
         </Content>
         <Item>
-          <img src={Operador} alt="Imagem Genérica" />
+          <img src={Operador} alt="Imagem operador" />
         </Item>
       </Row>
       <Row className="revertRow">
@@ -44,7 +44,7 @@ const Operadores = () => {
           <p>Acompanhe a quantidade de operadores, classes, jogos e etc...</p>
         </Content>
         <Item>
-          <img src={Capitao} alt="Imagem Genérica" />
+          <img src={Capitao} alt="Imagem capitao" />
         </Item>
       </Row>
       <Row>
@@ -60,22 +60,7 @@ const Operadores = () => {
           <p>Faça sua gestão financeira pelo app.</p>
         </Content>
         <Item>
-          <img src={Generic} alt="Imagem Genérica" />
-        </Item>
-      </Row>
-      <Row className="revertRow">
-        <Content>
-          <h2>Operações</h2>
-          <h4>Estatística</h4>
-          <p>Crie sua conta e comece a sua competição contra seu pior adversário...
-            Você... Gere estatísticas de equipe e suas operações</p>
-          <h4>Equipamento</h4>
-          <p>Cadastre o seu Loadout e compartilhe com seus amigos.</p>
-          <h4>Suba seu nível</h4>
-          <p>Conquiste novos níveis evolua em combate.</p>
-        </Content>
-        <Item>
-          <img src={Generic} alt="Imagem Genérica" />
+          <img src={organizador} alt="Imagem operador" />
         </Item>
       </Row>
     </Container>
